b6bdbeefe481386e3edda4e9ddcf5a9b757ca755
周逸群 <zhouyiqun@qianka.com>
2020-10-22 13:31:16 +0800

    feat:taro demo

---
+++ src/pages/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="index">
+    <button openType="getUserInfo" onGetUserInfo="getInfo">授权登录</button>
+  </view>
+</template>
+
+<script>
+import Taro from '@tarojs/taro'
+import './index.scss'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  },
+  methods: {
+    // 触发不了
+    getInfo(res) {
+      console.log('res', res)
+    }
+  },
+  mounted() {
+    // 授权后正常
+    Taro.getUserInfo({
+      success: function(res) {
+        console.log('1111', res)
+      }
+    })
+  }
+}
+</script>
